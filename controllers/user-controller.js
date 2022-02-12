7aa765fa8e7a288db3bcd4b19c418a281e3f6d17
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-02-11 21:25:12 -0500

    simplify post handler

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,34 +10,14 @@ const userController = {
         console.log("userGetSingle");
     },
     
-    // userPostNew(req, res) {
-    //     console.log("userPostNew");
-    //     const body = req.body;
-    //     console.log(JSON.stringify(body));
-    //     user.create(body)
-    //         .then(dbUserData => res.json(dbUserData))
-    //         .catch(err => res.status(400).json(err));    
-    // },    
-
     userPostNew(req, res) {
         console.log("userPostNew");
         const body = req.body;
         console.log(JSON.stringify(body));
-        
-        const success = (dbUserData) => {
-            console.log('success');
-            res.json(dbUserData);
-        };
-        
-        const failure = (error) => {
-            console.log('failure');
-            console.log(JSON.stringify(error, null, 4));
-            res.status(400).json(error);
-        }
-        
-        user.create(body).then(success).catch(failure);
-    },
-                
+        user.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));    
+    },    
     
     userPutModified(req, res) {
         console.log("userPutModified");
